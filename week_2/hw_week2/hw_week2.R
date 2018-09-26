7b1deda58dcba4e21be3662699ac18bfdd27d676
yitingpeng <b04303008@ntu.edu.tw>
2018-09-26 17:55:50 +0800

    revisement

--- week_2/hw_week2/hw_week2.R
+++ week_2/hw_week2/hw_week2.R
@@ -6,10 +6,21 @@ library(httr)
 library(xml2)
 library(rvest)
 
+
 url <-"https:/ecshweb.pchome.com.tw/search/v3.3/all/results?q=Microsoft%20surface&page=1&sort=rnk/dc"
 res <- GET(url)
 res
 res_json <- content(res)
 res_json
 do.call(rbind,res_json$prods)
+View(data.frame(do.call(rbind,res_json$prods)))
+
+url <-"https:/ecshweb.pchome.com.tw/search/v3.3/all/results?q=Microsoft%20surface&page=2&sort=rnk/dc"
+res <- GET(url)
+res
+res_json <- content(res)
+res_json
+do.call(rbind,res_json$prods)
+df<-data.frame(do.call(rbind,res_json$prods))
+df
 View(data.frame(do.call(rbind,res_json$prods)))
