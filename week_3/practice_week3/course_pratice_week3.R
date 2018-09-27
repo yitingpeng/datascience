40111db70a73e8751f7835f1d1f7f66ac11b9cea
yitingpeng <b04303008@ntu.edu.tw>
2018-09-27 11:01:01 +0800

    課堂練習3

---
+++ week_3/practice_week3/course_pratice_week3.R
@@ -0,0 +1,29 @@
+##week3_course practice
+
+##install and libary ggplot
+install.packages('ggplot2')
+library(ggplot2)
+
+##using data and change the form of the data
+
+d1<-women
+d1
+View(d1)
+summary(d1)
+
+##plot a photogram
+
+ggplot(d1, aes(x=height, y=weight))
+
+ggplot(d1, aes(x=height, y=weight))+
+  geom_point()
+
+ggplot(d1, aes(x=height, y=weight)) +
+  geom_point(color = "red", shape = 8)
+
+
+##try and error：it does not work because of lack of y
+##ggplot(d1, aes(x=height))
+##ggplot(d1, aes(x=height))+geom_point()
+##ggplot(d1, aes(x=height)) + geom_point(color = "red", shape = 8)
+
